e5032977df13003514d354d7e2738c1851624752
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-02-13 14:49:38 +0100

    Export header and footer to seperate files

--- src/index.php
+++ src/index.php
@@ -20,69 +20,58 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>PHP Book Manager</title>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="icon" href="favicon.png" />
-</head>
-<body>
-    <main>
-        <h1>PHP Book Manager</h1>
+<?php require 'layout/header.php'; ?>
 
-        <form method="get" class="search-form">
-            <label for="book-name">Search for a book:</label>
-            <input
-                id="book-name"
-                value="<?php echo $nameInQuery ? htmlspecialchars($bookName) : ''; ?>"
-                name="name"
-            />
-            <input type="submit" value="Search" />
-        </form>
+<form method="get" class="search-form">
+    <label for="book-name">Search for a book:</label>
+    <input
+        id="book-name"
+        value="<?php echo $nameInQuery ? htmlspecialchars($bookName) : ''; ?>"
+        name="name"
+    />
+    <input type="submit" value="Search" />
+</form>
 
+<?php
+    if ($books === [])
+    {
+        if ($nameInQuery)
+        {
+            echo 'No such book found!';
+        }
+        else
+        {
+            echo 'No books in the database!';
+        }
+    }
+    else
+    {
+?>
+<table>
+    <tbody>
+        <tr>
+            <th>Name</th>
+            <th>Author</th>
+            <th>Genre</th>
+            <th>Year</th>
+        </tr>
         <?php
-            if ($books === [])
-            {
-                if ($nameInQuery)
-                {
-                    echo 'No such book found!';
-                }
-                else
-                {
-                    echo 'No books in the database!';
-                }
-            }
-            else
+            foreach ($books as $book)
             {
         ?>
-        <table>
-            <tbody>
-                <tr>
-                    <th>Name</th>
-                    <th>Author</th>
-                    <th>Genre</th>
-                    <th>Year</th>
-                </tr>
-                <?php
-                    foreach ($books as $book)
-                    {
-                ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($book['name']); ?></td>
-                    <td><?php echo htmlspecialchars($book['author']); ?></td>
-                    <td><?php echo htmlspecialchars($book['genre']); ?></td>
-                    <td><?php echo htmlspecialchars($book['year']); ?></td>
-                </tr>
-                <?php
-                    }
-                ?>
-            </tbody>
-        </table>
+        <tr>
+            <td><?php echo htmlspecialchars($book['name']); ?></td>
+            <td><?php echo htmlspecialchars($book['author']); ?></td>
+            <td><?php echo htmlspecialchars($book['genre']); ?></td>
+            <td><?php echo htmlspecialchars($book['year']); ?></td>
+        </tr>
         <?php
             }
         ?>
-    </main>
-</body>
-</html>
+    </tbody>
+</table>
+<?php
+    }
+?>
+
+<?php require 'layout/footer.php'; ?>
