d27564036321376337d5174b225f5e354ff417c6
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-02-13 18:33:45 +0100

    Don't display genre or year if they're not present

--- src/book.php
+++ src/book.php
@@ -22,15 +22,32 @@
 <p>
     <b>Book name: </b><?php echo htmlspecialchars($book['name']) ?>
 </p>
+
 <p>
     <b>Author: </b><?php echo htmlspecialchars($book['author']) ?>
 </p>
+
+<?php
+if ($book['genre'])
+{
+?>
 <p>
     <b>Genre: </b><?php echo htmlspecialchars($book['genre']) ?>
 </p>
+<?php
+}
+?>
+
+<?php
+if ($book['year'])
+{
+?>
 <p>
     <b>Release year: </b><?php echo htmlspecialchars($book['year']) ?>
 </p>
+<?php
+}
+?>
 
 <?php require 'layout/footer.php'; ?>
 
