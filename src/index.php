f3b966152abdb943ca8b1844ec338d9c1f0033ba
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-02-13 13:17:31 +0100

    Add some CSS

--- src/index.php
+++ src/index.php
@@ -3,29 +3,33 @@
 <head>
     <title>PHP Book Manager</title>
     <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-    <table>
-        <tbody>
-            <tr>
-                <th>Name</th>
-                <th>Author</th>
-                <th>Genre</th>
-                <th>Year</th>
-            </tr>
-            <tr>
-                <td>Test book</td>
-                <td>John Doe</td>
-                <td>Thriller</td>
-                <td>2021</td>
-            </tr>
-            <tr>
-                <td>Testowa książka</td>
-                <td>Jan Kowalski</td>
-                <td>Fantasy</td>
-                <td>1999</td>
-            </tr>
-        </tbody>
-    </table>
+    <main>
+        <h1>PHP Book Manager</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Author</th>
+                    <th>Genre</th>
+                    <th>Year</th>
+                </tr>
+                <tr>
+                    <td>Test book</td>
+                    <td>John Doe</td>
+                    <td>Thriller</td>
+                    <td>2021</td>
+                </tr>
+                <tr>
+                    <td>Testowa książka</td>
+                    <td>Jan Kowalski</td>
+                    <td>Fantasy</td>
+                    <td>1999</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
 </body>
 </html>
