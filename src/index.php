3fe436bed8ddbfa01963e99be284601c4516aa4b
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-02-13 12:01:05 +0100

    Create example table in index.php

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Book Manager</title>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <table>
+        <tbody>
+            <tr>
+                <th>Name</th>
+                <th>Author</th>
+                <th>Genre</th>
+                <th>Year</th>
+            </tr>
+            <tr>
+                <td>Test book</td>
+                <td>John Doe</td>
+                <td>Thriller</td>
+                <td>2021</td>
+            </tr>
+            <tr>
+                <td>Testowa książka</td>
+                <td>Jan Kowalski</td>
+                <td>Fantasy</td>
+                <td>1999</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
